8b47b6512d55153c2661d7d7df4d57831e00f542
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2022-11-11 15:12:53 +0100

    Create basic template, add style.css and basic math operations

---
+++ main.js
@@ -0,0 +1,11 @@
+class Calculator {
+  static add = (a, b) => console.log(a + b);
+  static substract = (a, b) => console.log(a - b);
+  static multipy = (a, b) => console.log(a * b);
+  static divide = (a, b) => console.log(a / b);
+}
+
+Calculator.add(1, 2);
+Calculator.substract(1, 2);
+Calculator.multipy(1, 2);
+Calculator.divide(1, 2);
