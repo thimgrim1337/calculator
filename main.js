ef22ac3c1046e46929298f267aab04ef958bfe72
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2022-11-16 17:22:07 +0100

    Rebuild app

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
 class Calculator {
-  _a = undefined;
-  _b = undefined;
-  _result = 0;
-
-  static sum = (a, b) => a + b;
-  static substract = (a, b) => a - b;
-  static multiply = (a, b) => a * b;
-  static divide = (a, b) => a / b;
-
-  static operate(a, b, cb) {
-    return cb(a, b);
-  }
+  constructor() {}
 }
 
 class Event {
-  static captureInput() {
-    this.calcButtons = document.querySelectorAll('button');
-
-    this.calcButtons.forEach((button) =>
-      button.addEventListener('click', (e) => {
-        console.log(e.target.textContent);
-      })
-    );
-  }
-
-  static init() {
-    this.captureInput();
-  }
+  static captureEvent = () => {};
 }
 
-Event.init();
+Event.numberButtons = document.querySelectorAll('[data-number]');
+Event.operationButtons = document.querySelectorAll('[data-operator]');
+Event.equalsButton = document.querySelector('[data-operate]');
+Event.deleteButton = document.querySelector('[data-cancel');
+Event.allClearButton = document.querySelector('[data-clear]');
+Event.prevOperand = document.querySelector('.prevOperand');
+Event.currentOperand = document.querySelector('.currentOperand');
