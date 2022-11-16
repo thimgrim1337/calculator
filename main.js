78b70beeec47f2b723ad83ee0f1108633d57a999
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2022-11-16 21:55:48 +0100

    Create working operate and basic math operations methods

--- main.js
+++ main.js
@@ -14,11 +14,43 @@ class Calculator {
     if (this.currentOperand === '') return;
     if (this.prevOperand !== '') this.operate();
     this.operator = operator;
-    this.prevOperand = this.currentOperand;
+    this.prevOperand = `${this.currentOperand} ${this.operator}`;
     this.currentOperand = '';
   };
 
-  static operate = () => {};
+  static sum = (a, b) => a + b;
+  static substract = (a, b) => a - b;
+  static multiply = (a, b) => a * b;
+  static divide = (a, b) => (b != 0 ? a / b : alert('You cannot divide by 0'));
+  static modulo = (a, b) => a % b;
+
+  static operate = () => {
+    let result;
+    const prev = parseFloat(this.prevOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operator) {
+      case '+':
+        result = this.sum(prev, current);
+        break;
+      case '-':
+        result = this.substract(prev, current);
+        break;
+      case '*':
+        result = this.multiply(prev, current);
+        break;
+      case '/':
+        result = this.divide(prev, current);
+        break;
+      case '%':
+        result = this.modulo(prev, current);
+        break;
+    }
+    this.prevOperand = '';
+    this.currentOperand = result;
+  };
+
+  static clearCurrent = () => (this.currentOperand = '');
 
   static reset = () => {
     this.prevOperand = '';
@@ -35,12 +67,28 @@ class Event {
         UI.updateDisplay();
       })
     );
+
     this.operationButtons.forEach((button) =>
       button.addEventListener('click', () => {
         Calculator.setOperator(button.innerText);
         UI.updateDisplay();
       })
     );
+
+    this.equalsButton.addEventListener('click', () => {
+      Calculator.operate();
+      UI.updateDisplay();
+    });
+
+    this.allClearButton.addEventListener('click', () => {
+      UI.resetDisplay();
+      Calculator.reset();
+    });
+
+    this.deleteButton.addEventListener('click', () => {
+      UI.delete();
+      Calculator.clearCurrent();
+    });
   };
 
   static init = () => {
@@ -53,12 +101,21 @@ class UI {
     Event.currentOperand.innerText = Calculator.currentOperand;
     Event.prevOperand.innerText = Calculator.prevOperand;
   };
+
+  static resetDisplay = () => {
+    Event.currentOperand.innerText = '';
+    Event.prevOperand.innerText = '';
+  };
+
+  static delete = () => {
+    Event.currentOperand.innerText = '';
+  };
 }
 
 Event.numberButtons = document.querySelectorAll('[data-number]');
 Event.operationButtons = document.querySelectorAll('[data-operator]');
 Event.equalsButton = document.querySelector('[data-operate]');
-Event.deleteButton = document.querySelector('[data-cancel');
+Event.deleteButton = document.querySelector('[data-delete');
 Event.allClearButton = document.querySelector('[data-clear]');
 Event.prevOperand = document.querySelector('.prevOperand');
 Event.currentOperand = document.querySelector('.currentOperand');
