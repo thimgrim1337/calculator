ff85af8bb5e075977e044c7628f9ec895f6992a1
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2022-11-16 20:51:25 +0100

    Create working appendNumber and updateDisplay methods

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 class Calculator {
-  constructor() {}
+  static setOperand = (prevOperand, currentOperand) => {
+    this.prevOperand = prevOperand;
+    this.currentOperand = currentOperand;
+    this.reset();
+  };
+
+  static appendNumber = (number) => {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  };
+
+  static setOperator = (operator) => {
+    this.operator = operator;
+    console.log(this.operator);
+  };
+
+  static reset = () => {
+    this.prevOperand = '';
+    this.currentOperand = '';
+    this.operator = undefined;
+  };
 }
 
 class Event {
-  static captureEvent = () => {};
+  static captureEvent = () => {
+    this.numberButtons.forEach((button) =>
+      button.addEventListener('click', () => {
+        Calculator.appendNumber(button.innerText);
+        UI.updateDisplay();
+      })
+    );
+    this.operationButtons.forEach((button) =>
+      button.addEventListener('click', () => {
+        Calculator.setOperator(button.innerText);
+      })
+    );
+  };
+
+  static init = () => {
+    this.captureEvent();
+  };
+}
+
+class UI {
+  static updateDisplay = () =>
+    (Event.currentOperand.innerText = Calculator.currentOperand);
 }
 
 Event.numberButtons = document.querySelectorAll('[data-number]');
@@ -13,3 +54,7 @@ Event.deleteButton = document.querySelector('[data-cancel');
 Event.allClearButton = document.querySelector('[data-clear]');
 Event.prevOperand = document.querySelector('.prevOperand');
 Event.currentOperand = document.querySelector('.currentOperand');
+
+Calculator.setOperand(Event.prevOperand, Event.currentOperand);
+
+Event.init();
